d006e9d76ae4e6e41b5d588311d77756aaa422e1
金井建斗 <macos@macosnoMacBook-Pro.local>
2023-05-28 17:54:10 +0900

    add:ジェネリクス

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
-const message: string = "Hello World"
+function repeat<T>(element: T, length: number): T[] {
+  const result: T[] = [];
+  for (let i = 0; i < length; i++) {
+    result.push(element);
+  }
+  return result;
+}
 
-console.log(message);
+console.log(repeat<string>("a", 5));
+console.log(repeat<number>(123, 3));
