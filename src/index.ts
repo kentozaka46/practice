a6de0263dd60b3735dae9b30f91f9d36c9b1a1fa
金井建斗 <macos@macosnoMBP.lan>
2023-06-04 08:58:30 +0900

    add:mapped types

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
-// 可変長タプル型
-// 配列型はタプル型の中で1回しか使えない
-type NumberAndString = [number, ...string[], number];
+type Fruit = "apple" | "orange" | "strawberry";
 
-const arr1: NumberAndString = [25, "uhyo", "hyo", 25];
-const arr2: NumberAndString = [25, 25];
+type FruitNumbers = {
+  [P in Fruit]: number;
+};
+
+const numbers: FruitNumbers = {
+  apple: 3,
+  orange: 10,
+  strawberry: 20,
+};
