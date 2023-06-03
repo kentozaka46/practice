c35cdac66b7db3a5a1a062998bf965b9b2245ead
金井建斗 <macos@macosnoMBP.lan>
2023-06-04 07:51:29 +0900

    add:ユーザー定義型ガード

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,38 @@
-// 配列リテラルの型推論結果を配列型ではなくタプル型にする
-// オブジェクトリテラルから推論されるオブジェクト型は全てのプロパティがreadonlyになる
-const names1 = ["uhyo", "John", "Taro"];
-const names2 = ["uhyo", "John", "Taro"] as const;
+function isStringOrNumber(value: unknown): value is string | number {
+  return typeof value === "string" || typeof value === "number";
+}
 
-type Name = (typeof names2)[number];
+const something: unknown = 123;
+
+if (isStringOrNumber(something)) {
+  console.log(something.toString());
+}
+
+type Human = {
+  type: "Human";
+  name: string;
+  age: number;
+};
+
+function isHuman(value: any): value is Human {
+  if (value === null) return false;
+  return (
+    value.type === "Human" &&
+    typeof value.name === "string" &&
+    typeof value.age === "number"
+  );
+}
+
+function assertsHuman(value: any): asserts value is Human {
+  if (value === null) {
+    throw new Error("nullです");
+  }
+
+  if (
+    value.type !== "Human" ||
+    typeof value.name !== "string" ||
+    typeof value.age !== "number"
+  ) {
+    throw new Error("Humanではない");
+  }
+}
